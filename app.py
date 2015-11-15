eb14c1c6b3ae96cf8d1474815bab811b3d17eda9
Al Sos <dfjkhgshakazulu3091@gmail.com>
2015-11-14 20:35:49 -0500

    Twitter data is now available on the homepage.

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
-from flask import Flask, render_template, session, redirect,url_for
+from flask import Flask, render_template, session, redirect,url_for, request
 
-
+import twitter
 
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/home")
-@app.route("/index")
+@app.route("/",methods=["GET","POST"])
 def index():
-    return render_template("index.html")                 
+    if request.method=="POST":
+        twitter.search(request.form['searchterm'])
+        f = open('tweets.csv','r')
+        tweets = f.read()
+        f.close()
+        tweets = tweets.decode('utf-8')
+        return render_template("index.html",twitter=tweets)       
+    return render_template("index.html",twitter='No Search Has Been Done')                 
 
 
 if __name__ == "__main__":
-   app.debug = True
-   app.secret_key="Don't store this on github"
-   app.run(host="0.0.0.0", port=8000)
+    app.debug = True
+    app.secret_key="Don't store this on github"
+    app.run(host="0.0.0.0", port=8000)
--- twitter.py
+++ twitter.py
@@ -8,6 +8,15 @@ consumer_secret="xn3nmwcJEU6hfoH7NND8zX2aYbKwD3iEIQjlJFr6ouubWRNrJg"
 access_token="925616599-u6yMmcLB3Ar9RZq80e0wUTCZjZp5TotXapkTOxyQ"
 access_token_secret="E5DTEg4DGFmFGTKbCusko1tfjPgpWEvWLw68b6MCi64EB"
 
+"""
+search(hashtag)
+
+Input:
+hashtag - String that will be searched for.
+
+Output:
+Returns nothing, but writes search results in tweets.csv.
+"""
 def search(hashtag):
   auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
   auth.set_access_token(access_token, access_token_secret)
@@ -23,7 +32,7 @@ def search(hashtag):
         time    = tweet.created_at
         spamwriter.writerow((time, content))
 
-hashtag = raw_input("what hash do you want to search?")
+#hashtag = raw_input("what hash do you want to search?\n")
 
-search(hashtag)
-print 'success!'
+#search(hashtag)
+print 'Search Results Entered in tweets.csv'
