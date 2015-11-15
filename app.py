800ce0e0daba88b4caaddcf6d4701f86a87ac30c
Al Sos <dfjkhgshakazulu3091@gmail.com>
2015-11-15 18:48:43 -0500

    Finished NYT basics

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, session, redirect,url_for, request
 
-import twitter
+import twitter, nyt
 
 app = Flask(__name__)
 
@@ -8,12 +8,18 @@ app = Flask(__name__)
 def index():
     if request.method=="POST":
         twitter.search(request.form['searchterm'])
+        nyt.search(request.form['searchterm'])
         f = open('tweets.csv','r')
         tweets = f.read()
         f.close()
         tweets = tweets.decode('utf-8')
-        return render_template("index.html",twitter=tweets)       
-    return render_template("index.html",twitter='No Search Has Been Done')                 
+        g = open('articles.csv','r')
+        articles = g.read()
+        g.close()
+        articles = articles.decode('utf-8')
+        return render_template("index.html",twitter=tweets,nyt=articles)
+
+    return render_template("index.html",twitter='No Search Has Been Done',nyt='No Search Has Been Done')                 
 
 
 if __name__ == "__main__":
---
+++ nyt.py
@@ -0,0 +1,31 @@
+import csv
+import urllib2
+import json
+
+nyt_api_key = "&api-key=e1d08e31fabebf4e6b531b2ee8b4b922:9:73464677"
+url_init = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q="
+
+"""
+search(topic)
+
+Input:
+topic - String that will be searched for.
+
+Output:
+Returns nothing, but writes search results in articles.csv.
+"""
+
+def search(topic):
+    url = url_init+topic+nyt_api_key
+    request = urllib2.urlopen(url)
+    result = request.read()
+    r = json.loads(result)
+    s = r['response']
+    t = s['docs']
+    with open('articles.csv','wb') as csvfile:
+        spamwriter = csv.writer(csvfile)
+        for entry in t:
+            excerpt = (entry['snippet']+"<br>").encode('utf-8')
+            theurl = (entry['web_url']+"<br><br>").encode('utf-8')
+            spamwriter.writerow((excerpt,theurl))
+    print 'Search Results Entered in articles.csv'
