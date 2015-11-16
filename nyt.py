cf979ee8f0cf162cdc22bfbc661392a7779f3373
Al Sos <dfjkhgshakazulu3091@gmail.com>
2015-11-15 19:01:25 -0500

    Fixed space bug

--- nyt.py
+++ nyt.py
@@ -1,6 +1,6 @@
 import csv
 import urllib2
-import json
+import json,sys
 
 nyt_api_key = "&api-key=e1d08e31fabebf4e6b531b2ee8b4b922:9:73464677"
 url_init = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q="
@@ -16,16 +16,22 @@ Returns nothing, but writes search results in articles.csv.
 """
 
 def search(topic):
-    url = url_init+topic+nyt_api_key
-    request = urllib2.urlopen(url)
-    result = request.read()
-    r = json.loads(result)
-    s = r['response']
-    t = s['docs']
-    with open('articles.csv','wb') as csvfile:
-        spamwriter = csv.writer(csvfile)
-        for entry in t:
-            excerpt = (entry['snippet']+"<br>").encode('utf-8')
-            theurl = (entry['web_url']+"<br><br>").encode('utf-8')
-            spamwriter.writerow((excerpt,theurl))
-    print 'Search Results Entered in articles.csv'
+    contyn = True
+    for num in range(len(topic)):
+        if(topic[num]==" "):
+            search(topic[0:num]+"%20"+topic[num+1:len(topic)])
+            contyn = False
+    if(contyn==True):
+        url = url_init+topic+nyt_api_key
+        request = urllib2.urlopen(url)
+        result = request.read()
+        r = json.loads(result)
+        s = r['response']
+        t = s['docs']
+        with open('articles.csv','wb') as csvfile:
+            spamwriter = csv.writer(csvfile)
+            for entry in t:
+                excerpt = (entry['snippet']+"<br>").encode('utf-8')
+                theurl = (entry['web_url']+"<br><br>").encode('utf-8')
+                spamwriter.writerow((excerpt,theurl))
+                print 'Search Results Entered in articles.csv'
