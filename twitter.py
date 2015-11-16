2aa740980b1ba381462e24d1efbf757eded5600a
Liam Daly <liamgdalynyc@gmail.com>
2015-11-15 19:14:02 -0500

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -8,17 +8,17 @@ consumer_secret="xn3nmwcJEU6hfoH7NND8zX2aYbKwD3iEIQjlJFr6ouubWRNrJg"
 access_token="925616599-u6yMmcLB3Ar9RZq80e0wUTCZjZp5TotXapkTOxyQ"
 access_token_secret="E5DTEg4DGFmFGTKbCusko1tfjPgpWEvWLw68b6MCi64EB"
 
-"""
-search(hashtag)
-
-Input:
-hashtag - String that will be searched for.
-
-Output:
-Returns nothing, but writes search results in tweets.csv.
-"""
-
 def search(hashtag):
+  """
+  search(hashtag)
+
+  Input:
+  hashtag - String that will be searched for.
+
+  Output:
+  Returns nothing, but writes search results in tweets.csv.
+  
+  """
   auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
   auth.set_access_token(access_token, access_token_secret)
 
